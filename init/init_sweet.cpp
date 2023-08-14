0fc71500f5600d65440d7963f71a892545df6d0a
Niranjan BR <niranjankannan2003@gmail.com>
2023-08-14 05:31:52 +0000

    sweet: set the build fingerprint and description at build time
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- init/init_sweet.cpp
+++ init/init_sweet.cpp
@@ -81,24 +81,18 @@ void vendor_load_properties() {
 
     std::string model;
     std::string device;
-    std::string fingerprint;
-    std::string description;
     std::string mod_device;
     std::string marketname;
 
     if (region == "GLOBAL") {
         model = "M2101K6G";
         device = "sweet";
-        fingerprint = "Redmi/sweet_eea/sweet:13/RKQ1.210614.002/V14.0.5.0.TKFEUXM:user/release-keys";
-        description = "sweet_eea-user 13 RKQ1.210614.002 V14.0.5.0.TKFEUXM release-keys";
         marketname = "Redmi Note 10 Pro";
         mod_device = "sweet_eea_global";
         property_override("ro.boot.product.hardware.sku", "sweet");
     } else if (region == "JAPAN") {
         model = "M2101K6R";
         device = "sweet";
-        fingerprint = "Redmi/sweet_global/sweet:13/RKQ1.210614.002/V14.0.4.0.TKFMIXM:user/release-keys";
-        description = "sweet_global-user 13 RKQ1.210614.002 V14.0.4.0.TKFMIXM release-keys";
         marketname = "Redmi Note 10 Pro";
         mod_device = "sweet_global";
         property_override("ro.boot.product.hardware.sku", "sweet");
@@ -106,15 +100,11 @@ void vendor_load_properties() {
         if (sku == "std") {
             model = "M2101K6P";
             device = "sweetin";
-            fingerprint = "Redmi/sweetin/sweetin:13/RKQ1.210614.002/V14.0.1.0.TKFINXM:user/release-keys";
-            description = "sweetin-user 13 RKQ1.210614.002 V14.0.1.0.TKFINXM release-keys";
             marketname = "Redmi Note 10 Pro";
             mod_device = "sweetin_in_global";
         } else {
             model = "M2101K6I";
             device = "sweetin";
-            fingerprint = "Redmi/sweetinpro/sweetin:13/RKQ1.210614.002/V14.0.1.0.TKFINXM:user/release-keys";
-            description = "sweetinpro-user 13 RKQ1.210614.002 V14.0.1.0.TKFINXM release-keys";
             marketname = "Redmi Note 10 Pro Max";
             mod_device = "sweetin_in_global";
             }
@@ -123,7 +113,6 @@ void vendor_load_properties() {
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
     set_ro_product_prop("model", model);
-    property_override("ro.build.description", description.c_str());
     if (mod_device != "") {
         property_override("ro.product.mod_device", mod_device.c_str());
 
